c10c6d7a1822ff6212633449c0b961777745ff58
tmatth11 <tmmatthews11@gmail.com>
2024-06-27 21:00:35 -0500

    Fix error

--- src/components/SelectInput.jsx
+++ src/components/SelectInput.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SelectInput = ({ id, label, options, value, onChange, required = false }) => (
     <div className="mb-3">
         <label htmlFor={id} className="form-label fw-bold">{label}</label>
-        <select className="form-select" id={id} value={value} onChange={onChange} required={required}>
+        <select className="form-select" id={id} name={id} value={value} onChange={onChange} required={required}>
             {options.map(option => (
                 <option key={option.value} value={option.value}>{option.label}</option>
             ))}
